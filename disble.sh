4c47630f8d635bcdbf2aa5aaef05ad68a440346e
isahakukamil <38471175+isahakukamil@users.noreply.github.com>
2019-11-13 11:17:52 +0000

    Delete disble.sh

--- disble.sh
+++
@@ -1,9 +0,0 @@
-echo "************************************************************";
-echo "Python Script to Disable Root Login on Charging System Nodes";
-echo "************************************************************";
-echo > /etc/securetty;
-sed -i '/^PermitRootLogin[ \t]\+\w\+$/{s//PermitRootLogin no/g;}' /etc/ssh/sshd_config;
-echo "Root Login has been disabled Successfully";
-systemctl restart sshd;
-
-
